41c4ef72d4cef2c9a18b00085817da7b0014a728
Bo Aanes <boaanes@gmail.com>
2022-04-02 01:15:14 +0200

    extend bottle constructor

--- Main.hs
+++ Main.hs
@@ -21,12 +21,17 @@ instance ToJSON WineType where
 
 -- Data constructor for bottle
 data Bottle = Bottle
-    { bottleId :: Int           -- autogenerate?
-    , name :: String
-    , producer :: String
-    , wineType :: WineType
-    , vintage :: Maybe Int      -- if Nothing, implies it's a NV
-    , cost :: Maybe Int         -- if Nothing, cost is unknown (perhaps gift?)
+    { bottleId :: Int                   -- autogenerate?
+    , name :: String                    -- e.g. Romanée-Conti
+    , producer :: String                -- e.g. Domaine de la Romanée-Conti
+    , wineType :: WineType              -- e.g. Red
+    , country :: String                 -- e.g. France
+    , region :: String                  -- e.g. Burgundy
+    , subRegion :: Maybe String         -- e.g. Vosne-Romanée
+    , vineyard :: Maybe String          -- e.g. La Romanée-Conti
+    , vintage :: Maybe Int              -- e.g. 1998 - if Nothing, implies it's a NV
+    , cost :: Maybe Int                 -- e.g. 35000, price in NOK - if Nothing, cost is unknown (perhaps gift?)
+    , blend :: Maybe [(String, Int)]    -- e.g. [("Pinot Noir", 100)] or [("Montepulciano", 85), ("Merlot", 15)] list of grapes with percentage
     }
     deriving Generic
 
@@ -35,7 +40,7 @@ instance ToJSON Bottle where
     toEncoding = genericToEncoding defaultOptions
 
 getBottle :: Int -> Bottle
-getBottle bottleId = (Bottle bottleId "Quaranta" "Pennessi" Red (Just 2020) (Just 190))
+getBottle bottleId = (Bottle bottleId "Quaranta" "Pennessi" Red "Italy" "Tuscany" (Just "Montepulciano") Nothing (Just 2020) (Just 190) (Just [("Montepulciano", 85), ("Merlot", 15)]))
 
 main :: IO ()
 main = scotty 3000 $ do
