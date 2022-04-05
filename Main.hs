7a243531cbd034a5ab07a7e5ed45c7684e9b33f9
Bo Aanes <boaanes@gmail.com>
2022-04-06 00:08:45 +0200

    very WIP db work

--- Main.hs
+++ Main.hs
@@ -3,13 +3,18 @@
 
 import Web.Scotty
 
+import Control.Exception.Base
+
 import GHC.Generics
 import Data.Aeson.Types
 import Control.Applicative
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 import Database.SQLite.Simple.FromField
+import Database.SQLite.Simple.ToField
 
+import qualified Data.Text as T
+import qualified Data.Int as I
 
 -- Data constructor for wine type
 data WineType
@@ -22,23 +27,20 @@ instance FromJSON WineType
 instance ToJSON WineType where
     toEncoding = genericToEncoding defaultOptions
 instance FromField WineType where
-    fromField n = case n of
-            0 -> return Red
-            1 -> return White
-            _ -> return Sparkling
-{-
-    fromField _ mdata =
-        return wineType
-        where wineType = case mdata of
-                Just 0 -> Red
-                Just 1 -> White
-                _      -> Sparkling
--}
+    fromField field = case (fieldData field) of
+            (SQLText "Red")         -> return Red
+            (SQLText "White")       -> return White
+            (SQLText "Sparkling")   -> return Red
+            (SQLText other)         -> returnError Incompatible field ("Illegal value: '" ++ (T.unpack other) ++ "'")
+instance ToField WineType where
+    toField wineType = case wineType of
+            Red         -> (SQLText "Red")
+            White       -> (SQLText "White")
+            Sparkling   -> (SQLText "Sparkling")
 
 -- Data constructor for bottle
 data Bottle = Bottle
-    { bottleId :: Int                   -- autogenerate?
-    , name :: String                    -- e.g. Romanée-Conti
+    { name :: String                    -- e.g. Romanée-Conti
     , producer :: String                -- e.g. Domaine de la Romanée-Conti
     , wineType :: WineType              -- e.g. Red
     , country :: String                 -- e.g. France
@@ -54,10 +56,35 @@ instance FromJSON Bottle
 instance ToJSON Bottle where
     toEncoding = genericToEncoding defaultOptions
 instance FromRow Bottle where
-    fromRow = Bottle <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
+    fromRow = Bottle <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
+instance ToRow Bottle where
+    toRow (Bottle name producer wineType country region (Just subRegion) (Just vineyard) (Just vintage) (Just cost)) =
+        [(SQLText (T.pack name)), (SQLText (T.pack producer)), (toField wineType), (SQLText (T.pack country)), (SQLText (T.pack region)), (SQLText (T.pack subRegion)), (SQLText (T.pack vineyard)), (SQLInteger (fromIntegral vintage)), (SQLInteger (fromIntegral cost))]
 
 getBottle :: Int -> Bottle
-getBottle bottleId = (Bottle bottleId "Quaranta" "Pennessi" Red "Italy" "Tuscany" (Just "Montepulciano") Nothing (Just 2020) (Just 190))
+getBottle bottleId = (Bottle "Quaranta" "Pennessi" Red "Italy" "Tuscany" (Just "Montepulciano") Nothing (Just 2020) (Just 190))
+
+{-
+main :: IO ()
+main = do
+    conn <- open "wine.db"
+    r <- query_ conn "SELECT * FROM bottles" :: IO [Bottle]
+    mapM_ print r
+    close conn
+-}
+
+insertBottle :: Bottle -> IO ()
+insertBottle bottle = do
+    conn <- open "wine.db"
+    execute conn "INSERT INTO bottles (name, producer, wineType, country, region, subRegion, vineyard, vintage, cost) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)" bottle
+    close conn
+
+getBottleById :: Int -> IO ()
+getBottleById id = do
+    conn <- open "wine.db"
+    r <- queryNamed conn "SELECT * FROM bottles WHERE id = :id" [":id" := (id :: Int)]
+    mapM_ (mapM_ putStrLn) (r :: [[String]])
+    close conn
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -68,3 +95,4 @@ main = scotty 3000 $ do
   post "/bottle" $ do
     parsedJson <- jsonData :: ActionM Bottle
     json parsedJson
+
