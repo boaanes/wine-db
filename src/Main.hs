b51db046bcc4c71089baacd3a67a4667d552efcc
Bo Aanes <boaanes@gmail.com>
2022-05-06 17:29:09 +0200

    Add support for grape proportion
    still not solved field name bug

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,7 @@ initializeDB :: IO ()
 initializeDB = do
   conn <- open "wine.db"
   execute_ conn "CREATE TABLE IF NOT EXISTS bottles (id INTEGER PRIMARY KEY, polet_id INTEGER, name TEXT, producer TEXT, wine_type TEXT, country TEXT, district TEXT, sub_district TEXT, vineyard TEXT, vintage INTEGER, cost REAL)"
-  execute_ conn "CREATE TABLE IF NOT EXISTS grape_proportions (id INTEGER PRIMARY KEY, name TEXT, percentage REAL, bottle_id INTEGER)"
+  execute_ conn "CREATE TABLE IF NOT EXISTS grape_proportions (id INTEGER PRIMARY KEY, name TEXT, percentage REAL, bottle__id INTEGER)"
   close conn
 
 main :: IO ()
--- src/routes/Routes.hs
+++ src/routes/Routes.hs
@@ -2,6 +2,7 @@ module Routes where
 
 import           Control.Monad.IO.Class
 import           Data.Aeson                (decode)
+import           Data.Int
 import           Database
 import           Database.SQLite.Simple
 import           Network.HTTP.Simple
@@ -15,37 +16,46 @@ routes conn = do
   get "/" $ do
     bs <- liftIO $ getAllBottles conn
     json bs
+
   get "/:id" $ do
     bid <- param "id" :: ActionM Int
     b <- liftIO $ getBottleByID conn (fromIntegral bid)
     case b of
       Just bot -> json bot
       Nothing  -> status notFound404 >> raw "Bottle not found"
+
   get "/poletid/:id" $ do
     pid <- param "id" :: ActionM Int
     b <- liftIO $ getBottleByPoletID conn (fromIntegral pid)
     case b of
       Just bot -> json bot
       Nothing  -> status notFound404 >> raw "Bottle not found"
+
   post "/" $ do
     bottle <- jsonData :: ActionM Bottle
     liftIO $ insertBottle conn bottle
     json bottle
+
   post "/:id" $ do
     bid <- param "id" :: ActionM String
     poletResponse <- liftIO $ httpLbs $ parseRequest_ $ "http://www.vinmonopolet.no/api/products/" ++ bid ++ "?fields=FULL"
     case decode (getResponseBody poletResponse) :: Maybe PoletResponse of
+      Nothing          -> status notFound404 >> raw "Polet bottle not found"
       Just poletBottle -> do
         let bottle = fromPoletResponseToBottle poletBottle
-        let grapeProportions = fromPoletResponseToGrapeProportions poletBottle bottle
         liftIO $ insertBottle conn bottle
+
+        Just bottle' <- liftIO $ getBottleByPoletID conn (read bid :: Int32)
+        let grapeProportions = fromPoletResponseToGrapeProportions poletBottle bottle'
         liftIO $ insertGrapeProportions conn grapeProportions
+
         json bottle
-      Nothing          -> status notFound404 >> raw "Polet bottle not found"
+
   delete "/:id" $ do
     bid <- param "id" :: ActionM Int
     liftIO $ deleteBottleByID conn (fromIntegral bid)
     json ()
+
   put "/" $ do
     bottle <- jsonData :: ActionM Bottle
     liftIO $ updateBottle conn bottle
